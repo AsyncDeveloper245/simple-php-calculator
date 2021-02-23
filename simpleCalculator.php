bb6487eaec04d6dba93b63f305dc08cb2a3e5ca1
AsyncDeveloper245 <AsyncDeveloper245@users.noreply.github.com>
2021-02-23 08:50:26 +0100

    add calculator app

---
+++ simpleCalculator.php
@@ -0,0 +1,20 @@
+<?php
+function sum($a,$b){
+    return $a + $b;
+}
+function minus($a,$b){
+    return $a - $b;
+}
+
+function multiply($a,$b){
+    $a * $b;
+}
+
+function divide($a,$b){
+    return $a / $b;
+}
+echo sum(1,3);
+echo divide(1,3);
+echo multiply(1,3);
+echo minus(1,3);
+?>
